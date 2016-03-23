ef99c1b1f80e307fb05cd905a162c4f80542e6e2
Shazin Sadakath <shazin.sadakath@gmail.com>
2016-03-23 20:51:56 +0530

    Minor changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,17 @@
+// Developed by Shazin Sadakath
+
 $(document).ready(function(){
         var nm;
        $.ajax({
         type: "GET" ,
-        url: "rss.xml" ,
+        url: "http://shazin.github.io/rss.xml" ,
         dataType: "xml" ,
-        success: function(xml) {
-            alert(xml);
+        success: function(xmlDoc) {
+            $xml = $( xmlDoc )
+            var mainContent = $("#main-content");
+            $xml.find("channel item").each(function(index, element) {
+                mainContent.append('<div class="post"><h2>'+$(this).find("title").text()+'</h2>'+$(this).find("description").text()+'</div><p>See the original post at <a href="'+$(this).find("link").text()+'">'+$(this).find("link").text()+'</a></p><hr>');
+            });
         }
     });
 });
